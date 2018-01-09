08427a0ddc631b31ec025aaab6df950e0fe9cce2
NozomiSugiyama <rioc.sugiyama@gmail.com>
2018-01-09 11:29:45 +0900

    fix

--- src/app/struct/settlement.ts
+++ src/app/struct/settlement.ts
@@ -1,8 +1,8 @@
 import { Transition } from './transition';
 
 export class Settlement {
-    id       : number;
-    transiton: Transition[];
+    id          : number;
+    transiton   : Transition[];
     created_date: number;
     updated_date: number;
 }
