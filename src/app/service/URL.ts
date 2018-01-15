71607c0b58a717268dd7ebbebcf46843b4275aa0
NozomiSugiyama <rioc.sugiyama@gmail.com>
2018-01-15 21:57:25 +0900

    version  1.0.0

--- src/app/service/URL.ts
+++ src/app/service/URL.ts
@@ -1,22 +1,22 @@
 export const URL = {
-    // // dubug
-    "v1": {
-        auth       : "api/auth",
-        products   : "api/products",
-        settlements: "api/settlements",
-        self       : "api/self",
-        transition : "api/settlements/"
-    }
+    // dubug
+    // "v1": {
+    //     auth       : "api/auth",
+    //     products   : "api/products",
+    //     settlements: "api/settlements",
+    //     self       : "api/self",
+    //     transition : "api/settlements/"
+    // }
 
     // TODO Product Migration
     // Add origin when a real server is ready to receive requests.
 
-    // // product
-    // "v1": {
-    //     auth       : "https://is09api.acqq.xyz/api/auth",
-    //     products   : "https://is09api.acqq.xyz/api/products",
-    //     settlements: "https://is09api.acqq.xyz/api/settlements",
-    //     self       : "https://is09api.acqq.xyz/api/self",
-    //     transition : "https://is09api.acqq.xyz/api/settlements/"
-    // }
+    // product
+    "v1": {
+        auth       : "https://is09api.acqq.xyz/api/auth",
+        products   : "https://is09api.acqq.xyz/api/products",
+        settlements: "https://is09api.acqq.xyz/api/settlements",
+        self       : "https://is09api.acqq.xyz/api/self",
+        transition : "https://is09api.acqq.xyz/api/settlements/"
+    }
 }
