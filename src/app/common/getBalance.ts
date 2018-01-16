47b57e83247c18e070e2a7b8c2308cf65ba89e03
NozomiSugiyama <rioc.sugiyama@gmail.com>
2018-01-16 09:58:15 +0900

    fix

--- src/app/common/getBalance.ts
+++ src/app/common/getBalance.ts
@@ -1,10 +1,12 @@
 import { Self } from "../struct/self";
 import { Settlement } from "../struct/settlement";
 
+const sum = (prev, next) => prev + next;
+
 export default (self: Self, settlements: Settlement[]) : number => 
     self.amount - settlements.map(settlement => 
         settlement.transitions
             .map(x => x.price * x.count)
-            .reduce((prev, next) => prev + next, 0)
+            .reduce(sum, 0)
     )
-        .reduce((prev, next) => prev + next, 0)
+        .reduce(sum, 0)
